c6c685de8d621ea719529409a73729b8023fbfa9
Kavya6170 <kavyachougule6170@gmail.com>
2026-01-19 16:00:06 +0530

    exception, logger and utils added

--- src/MLOPs/utils/common.py
+++ src/MLOPs/utils/common.py
@@ -1,7 +1,7 @@
 import os
 from box.exceptions import BoxValueError
 import yaml
-from mlProject import logger
+from MLOPs import logger
 import json
 import joblib
 from ensure import ensure_annotations
